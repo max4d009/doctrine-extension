7a1493afbd9825f14c8d5f93432d6f68bb970d90
Maxim <max4d009@gmail.com>
2021-11-02 20:07:54 +0700

    Update ReferencesListener.php

--- src/Reference/ReferencesListener.php
+++ src/Reference/ReferencesListener.php
@@ -230,7 +230,7 @@ class ReferencesListener extends MappedEventSubscriber
         $meta = $om->getClassMetadata(get_class($object));
         $config = $this->getConfiguration($om, $meta->name);
 
-        if($config['referenceManyEmbed'])
+        if(isset($config['referenceManyEmbed']))
         foreach ($config['referenceManyEmbed'] as $mapping) {
             $property = $meta->reflClass->getProperty($mapping['field']);
             $property->setAccessible(true);
